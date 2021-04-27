ebc3d2492d9eef11eeee98efca82582ca22fadb0
lzs <59036386@qq.com>
2021-04-27 23:59:33 +0800

    新增接口讲师列表 新增接口逻辑删除讲师 新增common 父模块 service-base子模块 引入swagger 文档注解的基本使用

---
+++ service/service-edu/src/main/java/com/domoyun/eduservice/config/MyBatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.domoyun.eduservice.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.domoyun.eduservice.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * 逻辑删除插件
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+}
--- service/service-edu/src/main/java/com/domoyun/eduservice/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/domoyun/eduservice/controller/TeacherController.java
@@ -1,9 +1,16 @@
 package com.domoyun.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.domoyun.eduservice.entity.Teacher;
+import com.domoyun.eduservice.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +20,30 @@ import org.springframework.web.bind.annotation.RestController;
  * @author luozuanshi
  * @since 2021-04-27
  */
+@Api(tags = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class TeacherController {
+
+    @Autowired
+    TeacherService teacherService;
+
+    //查询讲师列表
+    @ApiOperation(value = "讲师列表",notes = "讲师管理讲师列表")
+    @GetMapping
+    public List<Teacher> list(){
+        List<Teacher> list = teacherService.list(null);
+        return list;
+    }
+
+    //删除讲师
+    @DeleteMapping("{id}")
+    @ApiOperation("删除指定讲师")
+    @ApiImplicitParams(@ApiImplicitParam(name = "id",value = "教师id",required = true,paramType ="form"))
+    public Boolean del(@PathVariable String id){
+        return teacherService.removeById(id);
+    }
+
 
 }
 
