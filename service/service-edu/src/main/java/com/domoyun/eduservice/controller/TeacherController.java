88cbcafbc0213e1aa6e1cfe5364637ef25aefbc1
lzs <59036386@qq.com>
2021-05-07 23:06:47 +0800

    1.新增common-utils 模块 新增统一返回结果对象 2.service 父依赖 引入 common-utils 模块 3.新增配置 分页插件 4.新增分页讲师列表

--- service/service-edu/src/main/java/com/domoyun/eduservice/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/domoyun/eduservice/controller/TeacherController.java
@@ -1,16 +1,17 @@
 package com.domoyun.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.domoyun.commonutils.R;
 import com.domoyun.eduservice.entity.Teacher;
 import com.domoyun.eduservice.service.TeacherService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -31,19 +32,36 @@ public class TeacherController {
     //查询讲师列表
     @ApiOperation(value = "讲师列表",notes = "讲师管理讲师列表")
     @GetMapping
-    public List<Teacher> list(){
+    public R list(){
         List<Teacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
     //删除讲师
     @DeleteMapping("{id}")
     @ApiOperation("删除指定讲师")
     @ApiImplicitParams(@ApiImplicitParam(name = "id",value = "教师id",required = true,paramType ="form"))
-    public Boolean del(@PathVariable String id){
-        return teacherService.removeById(id);
+    public R del(@PathVariable String id){
+        teacherService.removeById(id);
+        return R.ok();
     }
 
+    @ApiOperation("分页讲师列表")
+    @GetMapping("/{page}/{limit}")
+    public R pageList(@ApiParam(name = "page",value = "当前页码",required = true)
+                      @PathVariable Long page,
+                      @ApiParam(name = "limit",value = "每页记录数",required = true)
+                      @PathVariable Long limit){
+        Page<Teacher> pageParam = new Page<>(page,limit);
+        teacherService.page(pageParam,null);
+        List<Teacher> records = pageParam.getRecords();
+        long total = pageParam.getTotal();
+
+        Map<String,Object> pageMap = new HashMap();
+        pageMap.put("total",total);
+        pageMap.put("rows",records);
+        return R.ok().data(pageMap);
+    }
 
 }
 
